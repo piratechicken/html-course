60806aad4b944f17cdec58560889e7e26c9fc13a
piratechicken <simonadwyer@yahoo.com.au>
2018-02-26 18:44:27 +1100

    stuff

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,42 @@
 var marginLeft = 0;
-var marginShift = 10;
+var marginShift = 5;
 var marginTop = 0;
 var marginShiftTop = 20;
 invader = document.getElementById("spaceinvader");
 
 function startSpaceInvader() {
+  window.setInterval(moveInvader, 750);
+};
 
-  window.setInterval(moveInvader, 500);
+function switchInvader() {
+  if (invader.getAttribute('src') == './assets/img/spaceinvader.png') {
+    invader.src = './assets/img/spaceinvader1.png';
+  }
+  else {
+    invader.src = './assets/img/spaceinvader.png';
+  }
 };
 
 function moveInvader() {
-  const invader = document.getElementById("spaceinvader");
-
   if (marginLeft > 50) {
     marginShift = -10;
     marginTop = marginTop + marginShiftTop
+    invader.style.marginTop = `${marginTop}px`; 
+    marginLeft = marginLeft + marginShift;
+    invader.style.marginLeft = `${marginLeft}vw`;
+    switchInvader();
   } 
-  else if (marginLeft < 0)
-  {
+  else if (marginLeft < -50) {
     marginShift = +10;
     marginTop = marginTop + marginShiftTop
+    invader.style.marginTop = `${marginTop}px`; 
+    marginLeft = marginLeft + marginShift;
+    invader.style.marginLeft = `${marginLeft}vw`;
+    switchInvader();
   }
-  marginLeft = marginLeft + marginShift;
-  invader.style.marginLeft = `${marginLeft}vw`; 
-  invader.style.marginTop = `${marginTop}px`; 
-  // console.log("move");
+  else {
+    marginLeft = marginLeft + marginShift;
+    invader.style.marginLeft = `${marginLeft}vw`;
+    switchInvader();
+  }
 ;}
